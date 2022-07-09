9b8d3f7f345be7a83d3fb02cce1fb2816acdf4eb
Bojan <trb.bojan.privat@gmail.com>
2022-07-09 15:15:30 +0200

    Data Types

---
+++ 01_Basics/Variables-Three.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    // 1 Byte = 8 Bit
+    bool my_vlaue0 = true; // Can save true/false.
+
+    // The amount of byte varies on the compiler
+    // 1 Byte = 8 Bit
+    char my_value1 = 10;
+
+    // 2 Byte = 16 Bit
+    short my_value2 = 42;
+
+    // 4 Byte = 32 Bit
+    int my_value3 = 22; // 4 Byte
+
+    // 4 Byte = 32 Bit
+    float my_value4 = 12.0f; // The "f" at the end, signalises that this is a float.
+
+    // 8 Byte = 64 Bit
+    double my_value5 = 13.0;
+
+
+    return 0;
+}
